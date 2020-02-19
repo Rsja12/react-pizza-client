9929391a39e695468a644f98bc19a15d2cc3fc59
Rsja12 <asocarras10@gmail.com>
2020-02-19 16:55:18 -0500

    adds Navigation component and imports and renders in App.js

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <div className="ui inverted segment">
+            <div className="ui inverted secondary pointing menu">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navigation from '../components/Navigation'
 import './App.css'
 
 const App = () => {
     return (
         <div>
-            Testing
+            <Navigation />
         </div>
     )
 }
