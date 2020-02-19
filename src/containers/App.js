a33e58a9814a1d2570caea4546074fd101374b98
Rsja12 <asocarras10@gmail.com>
2020-02-19 15:45:47 -0500

    adds title component and imports it in App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Title from '../components/Title'
 import './App.css'
 
 const App = () => {
     return (
         <div>
-            
+            <Title />
         </div>
     )
 }
