27afa65087f5d527ff85e8fa5ed8c56863d94e3e
Rsja12 <asocarras10@gmail.com>
2020-02-19 16:50:01 -0500

    removes Title.js and Title.css

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Title from '../components/Title'
 import './App.css'
 
 const App = () => {
     return (
         <div>
-            <Title />
+            Testing
         </div>
     )
 }
