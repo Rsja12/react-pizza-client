ff071704f962a4fb6358a7931c5aaf1975c66063
Rsja12 <asocarras10@gmail.com>
2020-02-19 17:00:27 -0500

    adds Home item in navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,7 @@ const Navigation = () => {
     return (
         <div className="ui inverted segment">
             <div className="ui inverted secondary pointing menu">
+                <a className="item">Home</a>
                 
             </div>
         </div>
