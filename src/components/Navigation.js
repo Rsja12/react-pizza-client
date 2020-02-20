89730ca059a5064f9092603cf977a58437b8975b
Rsja12 <asocarras10@gmail.com>
2020-02-20 09:33:50 -0500

    updates comment

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = () => {
     return (
         <div className="ui inverted segment">
             <div className="ui inverted secondary pointing menu">
-                {/* Make all of these links their own components and route to their pages */}
+                {/* Make all of these links their own components and route to their pages and move them to the right side of the navbar */}
                 <a className="item">Home</a>
                 <a className="item">About</a>
                 <a className="item">Menu</a>
