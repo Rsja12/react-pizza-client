f72188027332e84bacc18bd1ece214eed8a57d15
Rsja12 <asocarras10@gmail.com>
2020-02-20 15:05:31 -0500

    imports About.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
 import Home from '../components/Home'
+import About from '../components/About'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 
