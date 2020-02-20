2b510a31b4b1cb17b4c21d48bc4d6c5ddb9028db
Rsja12 <asocarras10@gmail.com>
2020-02-20 15:06:11 -0500

    Adds route component, renders About

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,6 +11,7 @@ const App = () => {
             <div>
                 <Navigation />
                 <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
             </div>
         </Router>
     )
