b39e32375346ce61d2ede64c3dcfcb7c0a29ef8c
Rsja12 <asocarras10@gmail.com>
2020-02-20 15:12:19 -0500

    import NavLink

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Navigation.css'
+import { NavLink } from 'react-router-dom'
 
 const Navigation = () => {
     return (
         <div className="ui inverted segment">
             <div className="ui inverted secondary pointing menu">
-                {/* Make all of these links their own components and route to their pages and move them to the right side of the navbar */}
                 <a className="item">Home</a>
                 <a className="item">About</a>
                 <a className="item">Menu</a>
