d265215b05ba8e13a8bcce1e77d50340f72cea25
Rsja12 <asocarras10@gmail.com>
2020-02-27 11:23:45 -0500

    creates divs for displaying pizza name and description

--- src/components/PizzaItem.js
+++ src/components/PizzaItem.js
@@ -3,7 +3,10 @@ import React from 'react'
 const PizzaItem = () => {
     return (
         <div className="ui container center aligned">
-            
+            <div className="ui segment">
+                <div className="header"></div>
+                <div></div>
+            </div>
         </div>
     )
 }
