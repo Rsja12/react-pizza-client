3d2831c41bd89707959caee9f7a19dc44011d207
Rsja12 <asocarras10@gmail.com>
2020-02-27 11:18:44 -0500

    create store variable

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
 import App from './containers/App'
+import reducers from './reducers'
+
+const store = createStore(reducers, applyMiddleware(thunk))
  
 ReactDOM.render(
     <App />,
