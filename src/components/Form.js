a1a72444ae284129b04f3eadf30f22c1674427c7
Rsja12 <asocarras10@gmail.com>
2020-02-27 12:01:39 -0500

    import postPizza action creator

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 
+import { postPizza } from '../actions'
+
 export class Form extends Component {
     render() {
         return (
