5913de0f13a0b782b7d57f844c7a434918e19301
Rsja12 <asocarras10@gmail.com>
2020-02-29 16:39:08 -0500

    update HomeText

--- src/components/HomeText.js
+++ src/components/HomeText.js
@@ -3,7 +3,8 @@ import React from 'react'
 const HomeText = () => {
     return (
         <div>
-            <h1></h1>
+            <h1>Simply The Best Pizzas</h1>
+            <h1>Made by You!</h1>
         </div>
     )
 }
