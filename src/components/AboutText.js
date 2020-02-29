c865aab38af838e6cfa0310f890783eb7693668a
Rsja12 <asocarras10@gmail.com>
2020-02-29 16:15:29 -0500

    defines AboutText

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const AboutText = () => {
     return (
-        <div>
-            
+        <div style={{ marginLeft: 1000, marginTop: 300 }}>
+            <h2>Build your own pizza.</h2>
+            <h2>As many toppings as you want.</h2>
+            <h2>All for $9</h2>
         </div>
     )
 }
