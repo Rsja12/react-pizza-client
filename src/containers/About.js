3ff13c536c2a77a4d58ee1b7df25ba7a67d1e703
Rsja12 <asocarras10@gmail.com>
2020-02-29 16:16:42 -0500

    import AboutText in About

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
 import './About.css'
+import AboutText from '../components/AboutText'
 
 const About = () => {
     return (
         <div className="about">
             <Navigation />
+            <AboutText />
         </div>
     )
 }
