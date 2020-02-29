bb87d2822390623578069d2a12210d0d87a38664
Rsja12 <asocarras10@gmail.com>
2020-02-29 16:47:59 -0500

    update HomeText

--- src/components/HomeText.js
+++ src/components/HomeText.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const HomeText = () => {
+
     return (
-        <div>
+        <div style={{ color: "#FFF", marginTop: 100, marginLeft: 150 }}>
             <h1>Simply The Best Pizzas</h1>
             <h1>Made by You!</h1>
         </div>
