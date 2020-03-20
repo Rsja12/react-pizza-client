cb66b6f7995682c809ec76812926ff47a94eb50f
Rsja12 <asocarras10@gmail.com>
2020-03-20 15:26:35 -0400

    update AboutText component

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -3,9 +3,7 @@ import React from 'react'
 const AboutText = () => {
     return (
         <div style={{ marginLeft: 1040, marginTop: 290 }}>
-            <h2>Build Your Own Pizza</h2>
-            <h2>Fresh Ingredients</h2>
-            <h2>$8.99</h2>
+            <h2>Build Your Own Pizza<br />Fresh Ingredients<br />$8.99</h2>
         </div>
     )
 }
