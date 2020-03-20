55829a4829ed29fdeeb8e9738cba94b0594c6979
Rsja12 <asocarras10@gmail.com>
2020-03-20 16:52:52 -0400

    create and import Form.css

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { postPizza } from '../actions'
+import './Form.css'
 
 export class Form extends Component {
 
