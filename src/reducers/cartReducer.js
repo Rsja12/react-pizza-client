c06304f0160067542ed0226f30a8fce43c6de875
Rsja12 <asocarras10@gmail.com>
2020-03-26 16:22:35 -0400

    update cartReducer

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,9 +1,15 @@
-export default ( pizza = { name: '', description: [] }, action ) => {
+export default ( state = [], action ) => {
 
     switch( action.type ) {
         case 'ADD_TO_CART':
-            return action.payload
-        default:
+            // Returns a copy of the current array plus whatever object is being added 
+            return [...state, action.payload]
+        case 'REMOVE_FROM_CART':
+            // Finds index of first match 
+            const firstMatch = state.indexOf(action.payload)
+            // filters out obj with index === firstMatch
+            return state.filter( (obj, idx) => idx !== firstMatch )
+        default: 
             return pizza 
     }
 
