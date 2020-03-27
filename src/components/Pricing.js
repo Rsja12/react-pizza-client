0c9dbd2f160a4870bfce4d522949e4701b5b69a4
Rsja12 <asocarras10@gmail.com>
2020-03-27 17:41:13 -0400

    updates styling

---
+++ src/components/Pricing.js
@@ -0,0 +1,17 @@
+// component at bottom of cart route 
+// displays subtotal, tax and total and does the calculation of price for current items in cart
+
+import React from 'react'
+
+import '../styles/Pricing.css'
+
+const Pricing = (props) => {
+    console.log(props.pizza)
+    return (
+        <div className="price">
+            TESTING
+        </div>
+    )
+}
+
+export default Pricing
