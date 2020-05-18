56989fd76762cfa46415f4b5e24e28ff427c419d
Rsja12 <asocarras10@gmail.com>
2020-05-18 10:06:41 -0400

    implement uuid for cart items

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import { v4 as uuidv4 } from 'uuid'
 
 export default ( state = {}, action ) => {
 
@@ -7,6 +8,7 @@ export default ( state = {}, action ) => {
     switch( action.type ) {
 
         case 'ADD_TO_CART':
+            pizzaObj.id = uuidv4()
             pizzaObj.quantity = pizzaObj.quantity + 1 || 1
             return { ...state, [pizzaObj.id]: pizzaObj }
             
